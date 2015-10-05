8c6ea1878b58379862bc593e3e2cac52d7935f29
Julien SORET <julien@soret.in>
2015-10-05 11:43:42 +0200

    First version of AddBookActivity

--- PJE3JX/app/src/main/java/fr/univ_lille1/pje/pje3jx/Book.java
+++ PJE3JX/app/src/main/java/fr/univ_lille1/pje/pje3jx/Book.java
@@ -2,6 +2,8 @@ package fr.univ_lille1.pje.pje3jx;
 
 import android.graphics.Color;
 
+import java.util.Random;
+
 public class Book {
 
     private String name;
@@ -14,6 +16,19 @@ public class Book {
         this.author = author;
         this.image = image;
         this.genre = genre;
+    }
+
+    public Book(String name, String author, String genre) {
+        this(
+                name,
+                author,
+                Color.rgb(
+                        (int) ( Math.random() * 200) + 50,
+                        (int) ( Math.random() * 200) + 50,
+                        (int) ( Math.random() * 200) + 50
+                ),
+                genre)
+        ;
     }
 
     public String getName() {
